7f7382c631685b8e0928f4d97075547b37a9dd16
Ife Walter <ife@sangalo.com>
2016-10-11 14:00:53 +0100

    init yeoman

--- manage.py
+++ manage.py
@@ -1,48 +1,20 @@
-# -*- coding: utf-8 -*-
+#! /usr/bin/env python
+
+import os
 
 from flask.ext.script import Manager
-from muffinAPI import create_app
-from muffinAPI.extensions import db
 
-app = create_app()
+from muffin import create_app, db
+
+
+app = create_app(os.getenv('MUFFIN_CONFIG', 'default'))
 manager = Manager(app)
 
 
-@manager.command
-def run():
-    """Run in local machine."""
-
-    app.run(host='0.0.0.0', port=5000)
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db)
 
 
-@manager.command
-def initdb():
-    """Init/reset database."""
-
-    db.drop_all()
-    db.create_all()
-
-    # admin = User(
-    #         name=u'admin',
-    #         email=u'admin@example.com',
-    #         password=u'123456',
-    #         role_code=ADMIN,
-    #         status_code=ACTIVE,
-    #         user_detail=UserDetail(
-    #             sex_code=MALE,
-    #             age=10,
-    #             url=u'http://admin.example.com',
-    #             deposit=100.00,
-    #             location=u'Hangzhou',
-    #             bio=u'admin Guy is ... hmm ... just a admin guy.'))
-    # db.session.add(admin)
-    # db.session.commit()
-
-
-manager.add_option('-c', '--config',
-                   dest="config",
-                   required=False,
-                   help="config file")
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     manager.run()
